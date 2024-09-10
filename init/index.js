d04b136bec68095aad891a9196d46c454384a008
Varun Pandey <varunp5324@gmail.com>
2024-09-10 22:03:54 +0530

    adding file

--- init/index.js
+++ init/index.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose");
 const initdata=require("./data.js");
 const Product=require("../models/product.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/ECommerce";
+const MONGO_URL = "mongodb+srv://varunp5324:meyJXaruYXxjvogI@e-commerce.rb1rm.mongodb.net/?retryWrites=true&w=majority&appName=E-commerce";
 main().then(() => {
     console.log("connected to DB");
 })
@@ -15,7 +15,7 @@ async function main() {
 
 const initDB=async ()=>{
     await Product.deleteMany({});
-    initdata.data = initdata.data.map((obj) => ({...obj, owner: '66d6ae476102d60177fefaaa'}));
+    initdata.data = initdata.data.map((obj) => ({...obj, owner: '66e07315f44f5230c950b942'}));
     await Product.insertMany(initdata.data)
     console.log("data initialized");
 }
